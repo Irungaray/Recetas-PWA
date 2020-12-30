ffa98662ce33b3793ba19e8808e7073a9e0e16e1
Irungaray <lionelirungaray@gmail.com>
2020-12-30 20:46:57 -0300

    Applying Workbox Loading Strategies

--- src/service-worker.js
+++ src/service-worker.js
@@ -4,7 +4,34 @@
  * See https://goo.gl/S9QRab
  */
 
+// Estrategias de Workbox
+
 // Precarga la app
 self.__precacheManifest = [].concat(self.__precacheManifest || [])
 // workbox.precaching.suppressWarnings()
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {})
+
+workbox.routing.registerNavigationRoute('index.html')
+
+workbox.routing.registerRoute(/^https?:\/\/www.themealdb.com\/api\/.*/,
+  new workbox.strategies.StaleWhileRevalidate(),
+  'GET'
+)
+
+workbox.routing.registerRoute(
+  /^https:\/\/fonts.(?:googleapis|gstatic).com\/(.*)/,
+  workbox.strategies.cacheFirst({
+    cacheName: 'google-fonts-cache',
+    plugins: [
+      new workbox.expiration.Plugin({
+        maxAgeSeconds: 30 * 24 * 60 * 60
+      })
+    ]
+  }),
+  'GET'
+)
+
+// Reglas por defecto van abajo de todo, dado que sobrescriben
+workbox.routing.registerRoute(/^https?.*/,
+  workbox.strategies.networkFirst(), 'GET'
+)
